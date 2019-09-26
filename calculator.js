893133cb76cb94b7077b9135432d87e6ad461103
Wadie MENDJA <53305297+wadiemendja@users.noreply.github.com>
2019-09-26 16:48:29 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,17 @@
-Comming soon ..
+           function display(id) {
+                
+                let save = document.getElementById("actions").innerHTML ;
+                document.getElementById("actions").innerHTML=save+document.getElementById(id).innerHTML ;
+                save = document.getElementById("actions").innerHTML ;
+                
+            }
+            
+            function clean () {
+                document.getElementById("actions").innerHTML = "" ;
+                document.getElementById("screen").innerHTML = "0" ;
+            }
+            
+            function calc () {
+             document.getElementById("screen").innerHTML=eval(document.getElementById("actions").innerHTML);
+                
+            }
