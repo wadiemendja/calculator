e47556f0ca18c1e2161153902cc5010dc80adec4
Wadie Mendja <mendja2014@gmail.com>
2020-06-23 13:45:01 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,15 +1,12 @@
-           var actions = document.getElementById("actions").innerHTML;
-           var result = document.getElementById("screen").innerHTML;
-
            function display(id) {
-                actions+=document.getElementById(id).innerHTML ;
+                document.getElementById("actions").innerHTML+=document.getElementById(id).innerHTML ;
             }
             
             function clean () {
-                actions = "";
-                result = "0";
+                document.getElementById("actions").innerHTML="";
+                document.getElementById("screen").innerHTML="0";
             }
             
             function calc () {
-                result=eval(actions);                
+                document.getElementById("screen").innerHTML=eval(document.getElementById("actions").innerHTML);                
             }
